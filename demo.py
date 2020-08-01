c626d7103a44b21ef3625792d3c432b01705e5bf
mintaka33 <mintaka33@outlook.com>
2020-08-01 16:47:15 +0800

    update code

--- demo.py
+++ demo.py
@@ -3,22 +3,25 @@ import numpy as np
 
 print('OpenCV version: ', cv2.__version__)
 
-image = cv2.imread("test3.png")
-(h, w, _) = image.shape
-
-def compare_resize(image, w, h):
+def opencv_resize(image, w, h):
     img_area_x4 = cv2.resize(image, (w*4, h*4), interpolation=cv2.INTER_AREA)
-    cv2.imwrite('out_area_x4.bmp', img_area_x4)
+    cv2.imwrite('./image/out_area_x4.bmp', img_area_x4)
     img_cubic_x4 = cv2.resize(image, (w*4, h*4), interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite('out_cubic_x4.bmp', img_cubic_x4)
+    cv2.imwrite('./image/out_cubic_x4.bmp', img_cubic_x4)
     img_lanczos_x4 = cv2.resize(image, (w*4, h*4), interpolation=cv2.INTER_LANCZOS4)
-    cv2.imwrite('out_lanczos_x4.bmp', img_lanczos_x4)
+    cv2.imwrite('./image/out_lanczos_x4.bmp', img_lanczos_x4)
+
+image = cv2.imread("image/test_input.png")
+(h, w, _) = image.shape
+opencv_resize(image, w, h)
 
 model_path = "models/EDSR_x4.pb"
 model_name, model_scale = 'edsr', 4
 sr = cv2.dnn_superres.DnnSuperResImpl_create()
 sr.readModel(model_path)
 sr.setModel(model_name, model_scale)
+out_img = sr.upsample(image)
+cv2.imwrite('./image/out-edsr_x4.bmp', out_img)
 
 cap = cv2.VideoCapture('./video/test_input.mp4')
 if not cap.isOpened():
@@ -28,11 +31,12 @@ if not cap.isOpened():
 frame_count = 0
 while True:
     ret, frame = cap.read()
-    if ret == False:
+    if ret == False or frame_count > 1:
         break;
-    outimg = sr.upsample(frame)
-    cv2.imwrite('./out/edsrx4_%03d.bmp' % frame_count, outimg)
-    print('frame# %03d processed' % frame_count)
+    out_frame = sr.upsample(frame)
+    cv2.imwrite('./out/edsrx4_%03d.bmp' % frame_count, out_frame)
+    print('frame# %03d done' % frame_count)
     frame_count += 1
 
+cap.release()
 print('done')
