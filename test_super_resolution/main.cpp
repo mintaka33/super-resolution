31837cab8df14bcab47e23327d61105389c3abd3
lli108 <liang.a.li@intel.com>
2020-09-19 17:02:14 -0400

    add code

--- test_super_resolution/main.cpp
+++ test_super_resolution/main.cpp
@@ -5,11 +5,47 @@
 
 #include <inference_engine.hpp>
 #include <ie_version.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace InferenceEngine;
 
 string msr = "/home/fresh/data/model/single-image-super-resolution-1032.xml";
+string inputImgFile = "/home/fresh/data/work/decode_sr_encode/build/sr/test.png";
+
+template <typename T>
+void matU8ToBlob(const cv::Mat& orig_image, InferenceEngine::Blob::Ptr& blob, int batchIndex = 0) {
+    InferenceEngine::SizeVector blobSize = blob->getTensorDesc().getDims();
+    const size_t width = blobSize[3];
+    const size_t height = blobSize[2];
+    const size_t channels = blobSize[1];
+    InferenceEngine::MemoryBlob::Ptr mblob = InferenceEngine::as<InferenceEngine::MemoryBlob>(blob);
+    if (!mblob) {
+        THROW_IE_EXCEPTION << "We expect blob to be inherited from MemoryBlob in matU8ToBlob, "
+            << "but by fact we were not able to cast inputBlob to MemoryBlob";
+    }
+    // locked memory holder should be alive all time while access to its buffer happens
+    auto mblobHolder = mblob->wmap();
+
+    T *blob_data = mblobHolder.as<T *>();
+
+    cv::Mat resized_image(orig_image);
+    if (static_cast<int>(width) != orig_image.size().width ||
+            static_cast<int>(height) != orig_image.size().height) {
+        cv::resize(orig_image, resized_image, cv::Size(width, height));
+    }
+
+    int batchOffset = batchIndex * width * height * channels;
+
+    for (size_t c = 0; c < channels; c++) {
+        for (size_t  h = 0; h < height; h++) {
+            for (size_t w = 0; w < width; w++) {
+                blob_data[batchOffset + c * width * height + h * width + w] =
+                        resized_image.at<cv::Vec3b>(h, w)[c];
+            }
+        }
+    }
+}
 
 void infoIE(Core& ie, const string d)
 {
@@ -30,6 +66,7 @@ int main(int argc, char* argv[])
 {
     const string device = "CPU";
     const string inputBlobName = "0";
+    const string bicInputBlobName = "1";
 
     Core ie;
 
@@ -68,7 +105,60 @@ int main(int argc, char* argv[])
 
     ExecutableNetwork executableNetwork = ie.LoadNetwork(network, device);
     InferRequest inferRequest = executableNetwork.CreateInferRequest();
+
+    // low resoution input
     Blob::Ptr lrInputBlob = inferRequest.GetBlob(inputBlobName);
+    cv::Mat inputImg = cv::imread(inputImgFile, cv::IMREAD_COLOR);
+    if (inputImg.empty()) {
+        printf("ERROR: failed to load input impage file!\n");
+        return -1;
+    }
+    matU8ToBlob<float_t>(inputImg, lrInputBlob, 0);
+
+    // high resolution input from bicubic up-scaling
+    cv::Mat resizedImg;
+    Blob::Ptr bicInputBlob = inferRequest.GetBlob(bicInputBlobName);
+    int w2 = bicInputBlob->getTensorDesc().getDims()[3];
+    int h2 = bicInputBlob->getTensorDesc().getDims()[2];
+    cv::resize(inputImg, resizedImg, cv::Size(w2, h2), 0, 0, cv::INTER_CUBIC);
+    matU8ToBlob<float_t>(resizedImg, bicInputBlob, 0);
+
+    // do inference
+    inferRequest.Infer();
+
+    // process output
+    const Blob::Ptr outputBlob = inferRequest.GetBlob(firstOutputName);
+    LockedMemory<const void> outputBlobMapped = as<MemoryBlob>(outputBlob)->rmap();
+    const auto outputData = outputBlobMapped.as<float*>();
+    size_t numOfImages = outputBlob->getTensorDesc().getDims()[0];
+    size_t numOfChannels = outputBlob->getTensorDesc().getDims()[1];
+    size_t h3 = outputBlob->getTensorDesc().getDims()[2];
+    size_t w3 = outputBlob->getTensorDesc().getDims()[3];
+    size_t nunOfPixels = w3 * h3;
+
+    printf("INFO: Output size [N,C,H,W]: %d, %d, %d, %d\n", numOfImages, numOfChannels, h3, w3);
+
+    for (size_t i = 0; i < numOfImages; ++i) {
+        std::vector<cv::Mat> imgPlanes;
+        if (numOfChannels == 3) {
+            imgPlanes = std::vector<cv::Mat>{
+                  cv::Mat(h3, w3, CV_32FC1, &(outputData[i * nunOfPixels * numOfChannels])),
+                  cv::Mat(h3, w3, CV_32FC1, &(outputData[i * nunOfPixels * numOfChannels + nunOfPixels])),
+                  cv::Mat(h3, w3, CV_32FC1, &(outputData[i * nunOfPixels * numOfChannels + nunOfPixels * 2]))};
+        } else {
+            imgPlanes = std::vector<cv::Mat>{cv::Mat(h3, w3, CV_32FC1, &(outputData[i * nunOfPixels * numOfChannels]))};
+            // Post-processing for text-image-super-resolution models
+            cv::threshold(imgPlanes[0], imgPlanes[0], 0.5f, 1.0f, cv::THRESH_BINARY);
+        };
+
+        for (auto & img : imgPlanes)
+            img.convertTo(img, CV_8UC1, 255);
+    
+        cv::Mat resultImg;
+        cv::merge(imgPlanes, resultImg);
+        std::string outImgName = std::string("sr_" + std::to_string(i + 1) + ".png");
+        cv::imwrite(outImgName, resultImg);
+    }
 
     printf("\nExecution done!\n");
     return 0;
